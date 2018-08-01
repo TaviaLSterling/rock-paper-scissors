f532c7969a2ceceaaddd9b4de3aba356bf92eb5f
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-01 13:22:02 -0600

    create choice function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-function play(playerChoice){
+
     let compChoice = Math.random();
-    if (compChoice > .34) {
-        compChoice = "rock"};
-        else if(compChoice > .66) {
-            compChoice = "paper"
-        } else compChoice = "scissors"
-        
+    if (compChoice < .34) {
+        compChoice = "rock";}
+        else if(compChoice <= .67) {
+            compChoice = "paper";
+        } else{ compChoice = "scissors"
     }
-}
+    
+   var compare = function play(choice1,choice2){
+
+    }
