23cd95171fcb2897de9f082f366e3b4490160407
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-01 13:09:38 -0600

    computer choices

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function play(playerChoice){
-    
+    let compChoice = Math.random();
+    if (compChoice > .34) {
+        compChoice = "rock"};
+        else if(compChoice > .66) {
+            compChoice = "paper"
+        } else compChoice = "scissors"
+        
+    }
 }
