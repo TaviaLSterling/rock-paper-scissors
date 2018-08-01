766f8a81cdae2dbccf6b4b1264beadacad557a1c
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-01 13:47:38 -0600

    add images

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 
-    let compChoice = Math.random();
-    if (compChoice < .34) {
-        compChoice = "rock";}
-        else if(compChoice <= .67) {
-            compChoice = "paper";
-        } else{ compChoice = "scissors"
-    }
-    
-   var compare = function play(choice1,choice2){
+let compChoice = Math.random();
+if (compChoice < .34) {
+    compChoice = "rock";
+}
+else if (compChoice <= .67) {
+    compChoice = "paper";
+} else {
+    compChoice = "scissors"
+}
 
+var compare = function play(choice1, choice2) {
+    if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "Rock Wins!";
+        } else {
+            return "Paper Wins!";
+        }
+    } if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "Paper Wins!";
+        } else {
+            return "Scissors Wins!";
+        }
+    } if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            return "Rock Wins!";
+        } else {
+            return "Scissors Wins!";
+        }
     }
+};
+compare(userChoice,compChoice);
